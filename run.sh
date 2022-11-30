a99b3a5356c1bbe0e5d00bd751f495c063e24640
Ibiyemi Abiodun <ibiyemi@intulon.com>
2022-11-30 02:08:17 -0500

    omg audio buttons work

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 exec_name=`basename $1`
 scp $1 pi@iaa34.local:/home/pi
-ssh -t pi@iaa34.local "RUST_LOG=pidj=trace /home/pi/$exec_name"
+ssh -t -o SendEnv=RUST_LOG pi@iaa34.local /home/pi/$exec_name
--- src/audio.rs
+++ src/audio.rs
@@ -1,16 +1,12 @@
-use std::{
-    fs::File,
-    io::BufReader,
-    thread::JoinHandle,
-};
+use std::{fs::File, io::BufReader, thread::JoinHandle, time::Duration};
 
 use anyhow::Context;
-use indicatif::{ProgressBar, ParallelProgressIterator};
-use rodio::Decoder;
-use tracing::{debug, info, trace};
+use indicatif::{ParallelProgressIterator, ProgressBar, ProgressIterator, ProgressStyle};
 use rayon::prelude::*;
+use rodio::{Decoder, OutputStream, Source};
+use tracing::{debug, info, trace};
 
-use crate::keyboard;
+use crate::{driver::adafruit::seesaw::keypad::Edge, keyboard};
 
 #[derive(Debug, Clone, Copy)]
 pub enum Command {}
@@ -27,23 +23,59 @@ pub fn spawn_thread(kb_evt_rx: flume::Receiver<keyboard::Event>) -> JoinHandle<a
 
         debug!("globbing {glob_pattern:?}");
 
+        let pb_style = ProgressStyle::with_template(
+            "{prefix:>12.cyan.bold} [{spinner}] {pos}/{len} {wide_msg}",
+        )
+        .unwrap();
+
         let pb = ProgressBar::new_spinner();
-        let audio_files = globwalk::glob(glob_pattern)?
-            .par_bridge()
-            .progress_with(pb)
+        pb.set_style(pb_style);
+        pb.set_prefix("Locating");
+
+        let paths = globwalk::glob(glob_pattern)?
             .map(|entry| -> anyhow::Result<_> {
                 let entry = entry?;
                 let path = entry.path();
-                let file = File::open(path).context("failed to open audio file")?;
-                let reader = BufReader::new(file);
-                let decoder = Decoder::new(reader).context("failed to decode audio file")?;
-                trace!("loaded audio file {path:?}");
-                Ok(decoder)
+                pb.set_message(path.to_string_lossy().to_string());
+                Ok(path.to_path_buf())
             })
             .collect::<Result<Vec<_>, _>>()
-            .context("failed to load one or more audio files")?;
+            .context("failed to locate audio files")?;
 
-        info!("loaded {} audio files", audio_files.len());
+        pb.finish_with_message("Located audio files");
+
+        let (_stream, stream_handle) =
+            OutputStream::try_default().context("no audio output stream available")?;
+
+        loop {
+            match kb_evt_rx.recv() {
+                Ok(evt) => match evt {
+                    keyboard::Event::Key(evt) => match evt.edge {
+                        Edge::Rising => {
+                            let sound_idx = (evt.key.0 * 4 + evt.key.1) as usize;
+
+                            let path = match paths.get(sound_idx) {
+                                Some(path) => path,
+                                None => continue,
+                            };
+
+                            debug!("key {:?} pressed, playing sound from {path:?}", evt.key);
+
+                            let file = File::open(path).context("failed to open audio file")?;
+                            let reader = BufReader::new(file);
+                            let decoder =
+                                Decoder::new(reader).context("failed to decode audio file")?;
+
+                            stream_handle
+                                .play_raw(decoder.convert_samples())
+                                .context("failed to play sound")?;
+                        }
+                        _ => {}
+                    },
+                },
+                Err(_) => break,
+            }
+        }
 
         Ok(())
     })
